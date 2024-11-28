1891a94b711b764318d378a9ce3d5aba26949a52
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-11-28 12:40:34 +0700

    setup store

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Provider } from "react-redux";
 import "./App.css";
+import { store } from "./store/store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <div>App</div>
-    </>
+    </Provider>
   );
 }
 
