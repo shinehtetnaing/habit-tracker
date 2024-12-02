6da0d5fc181d42fd2c814d904cfa7f8b67536894
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-12-02 12:23:41 +0700

    display streak

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,16 +1,40 @@
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  LinearProgress,
+  Paper,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store/store";
-import { removeHabit, toggleHabit } from "../store/habitSlice";
+import { Habit, removeHabit, toggleHabit } from "../store/habitSlice";
 
 const HabitList: React.FC = () => {
   const { habits } = useSelector((state: RootState) => state.habits);
   const dispatch = useDispatch<AppDispatch>();
 
   const today = new Date().toISOString().split("T")[0];
+
+  const getStreak = (habit: Habit) => {
+    let streak = 0;
+    const currentDate = new Date();
+
+    while (true) {
+      const dateString = currentDate.toISOString().split("T")[0];
+      if (habit.completedDates.includes(dateString)) {
+        streak++;
+        currentDate.setDate(currentDate.getDate() - 1);
+      } else {
+        break;
+      }
+    }
+
+    return streak;
+  };
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
@@ -53,7 +77,7 @@ const HabitList: React.FC = () => {
             </Grid>
           </Grid>
 
-          {/* <Box sx={{ mt: 2 }}>
+          <Box sx={{ mt: 2 }}>
             <Typography variant="body2">
               Current Streak: {getStreak(habit)} days
             </Typography>
@@ -62,7 +86,7 @@ const HabitList: React.FC = () => {
               value={(getStreak(habit) / 30) * 100}
               sx={{ mt: 1 }}
             />
-          </Box> */}
+          </Box>
         </Paper>
       ))}
     </Box>
